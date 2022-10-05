220be9b9475efea524897bf730eb16e10ff2c561
boulguant <boulgant2000@gmail.com>
2022-10-06 00:27:12 +0100

    sm changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,25 @@
-const weatherForm = document.querySelector('form')
+console.log('hello weather')
+
+
+const weatherform = document.querySelector('form')
 const search = document.querySelector('input')
-const messageOne = document.querySelector('#message-1')
-const messageTwo = document.querySelector('#message-2')
+const message1 = document.querySelector('#message-1')
+const message2 = document.querySelector('#message-2')
 
-weatherForm.addEventListener('submit', (e) => {
+weatherform.addEventListener('submit',(e)=>{
     e.preventDefault()
+    message1.textContent='loading...'
+    fetch('/weather?address='+ search.value).then((response)=>{
+        response.json().then((data)=>{
+            if(data.error){
+                message1.textContent= data.error
+                return console.log(data.error)
 
-    const location = search.value
-
-    messageOne.textContent = 'Loading...'
-    messageTwo.textContent = ''
-
-    fetch('/weather?address=' + location).then((response) => {
-        response.json().then((data) => {
-            if (data.error) {
-                messageOne.textContent = data.error
-            } else {
-                messageOne.textContent = data.location
-                messageTwo.textContent = data.forecast
             }
+            message1.textContent='weather infos'
+            console.log(data)
+            console.log('la temperature dans '+ data.place+ ' est ' + data.temperature)
+            message2.textContent = 'la temperature dans '+ data.place+ ' est ' + data.temperature
         })
     })
 })
